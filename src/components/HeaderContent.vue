926be7386113a1e17553d62c9d50f383b529bd3a
susannasaadi <susannasaadi@gmail.com>
2020-12-27 21:18:36 +0200

    Header contents component style

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header-content">
-    <p class="title">Web developer<span>/</span>Student</p>
+    <p class="title"><span class="nb">Software developer</span><span class="tint-red"> /</span><br><span class="tab">Student</span>
+    </p>
     <p class="portfolio-title">PORTFOLIO</p>
     <img class="down-arrow" alt="scroll down image" src="../assets/down.svg">
   </div>
@@ -14,4 +15,54 @@ export default {
 
 <style scoped>
 
+.header-content {
+  position: absolute;
+  top: 65%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 35%;
+}
+
+.portfolio-title {
+  padding: 0;
+  color: #B53D4B;
+  font-family: Montserrat, sans-serif;
+  font-size: 14vw;
+  font-weight: 500;
+  text-transform: uppercase;
+}
+
+.title {
+  position: absolute;
+  top: -3em;
+  left: 1em;
+  padding: 0;
+  color: #FFF;
+  font-family: Montserrat, sans-serif;
+  font-size: 8vw;
+  font-weight: 400;
+  line-height: 1.2em;
+}
+
+.down-arrow {
+  position: absolute;
+  margin-bottom: -1.5em;
+  bottom: 0;
+}
+
+.nb {
+  white-space: nowrap;
+}
+
+.tint-red {
+  color: #B53D4B;
+}
+
+.tab {
+  display: inline-block;
+  margin-left: 6.7em;
+}
 </style>
