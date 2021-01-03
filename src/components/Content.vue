c4c03327a07986e21046dfc2cbe10ee08a050750
susannasaadi <susannasaadi@gmail.com>
2021-01-03 18:28:04 +0200

    ContentBlock pictures, msg and title logic
    Created Footer component

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="content">
 
-    <ContentBlock></ContentBlock>
-    <ContentBlock></ContentBlock>
-    <ContentBlock></ContentBlock>
+    <ContentBlock picUrl="flowers.jpg" title="Lorem Ipsum"
+                  msg=" In ac odio luctus, aliquam nibh nec, venenatis odio. Duis commodo arcu sed ex blandit, id fringilla augue pulvinar. Sed egestas, dui ut finibus maximus, ante augue euismod nisi, nec fermentum ligula enim non felis."></ContentBlock>
+    <ContentBlock picUrl="face.png" title="Lorem Ipsum"
+                  msg=" In ac odio luctus, aliquam nibh nec, venenatis odio. Duis commodo arcu sed ex blandit, id fringilla augue pulvinar. Sed egestas, dui ut finibus maximus, ante augue euismod nisi, nec fermentum ligula enim non felis."></ContentBlock>
+    <ContentBlock picUrl="circles.svg" title="Lorem Ipsum"
+                  msg=" In ac odio luctus, aliquam nibh nec, venenatis odio. Duis commodo arcu sed ex blandit, id fringilla augue pulvinar. Sed egestas, dui ut finibus maximus, ante augue euismod nisi, nec fermentum ligula enim non felis."></ContentBlock>
 
   </div>
 </template>
@@ -24,9 +27,9 @@ export default {
 
 
 <style scoped lang="scss">
-.content{
+.content {
   padding-top: 2vh;
   padding-bottom: 4vh;
-  background-image: linear-gradient(#020B17,#062041);
+  background-image: linear-gradient(#020B17, #062041);
 }
 </style>
--- src/components/contentblocks/ContentBlock.vue
+++ src/components/contentblocks/ContentBlock.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="contentBlock">
-    <div class="picture">
-      <button>Click me!</button>
+    <div class="picture" :style="{ 'backgroundImage': 'url(' + require('@/assets/' + picUrl) + ')'}">
+      <button v-on:click="btnClick">Click me!</button>
     </div>
-    <div class="description"><p><span>Lorem ipsum</span> dolor sit amet, consectetur adipiscing elit. Nunc vitae odio vitae ipsum efficitur elementum.</p></div>
+    <div class="description"><p><span>{{ title }}</span>{{ msg }}</p></div>
   </div>
 </template>
 
 <script>
-// import HeaderContent from '@/components/HeaderContent.vue'
 
 export default {
   name: 'ContentBlock',
-  components: {
-    // ContentBlock,
-  },
   props: {
-    // msg: String
+    msg: String,
+    title: String,
+    picUrl: String,
   },
+  methods: {
+    btnClick: function () {
+      alert("suvaline tekst");
+    }
+  }
 }
 </script>
 
 
 <style scoped lang="scss">
 .description {
-  font-family: Montserrat, sans-serif;;
+  font-family: Montserrat, sans-serif;
   font-size: 1.1em;
   line-height: 124%;
   font-weight: 400;
@@ -36,12 +39,13 @@ export default {
   align-items: center;
   color: #FFFFFF;
 }
+
 span {
   font-weight: 600;
 }
+
 .picture {
   height: 36vh;
-  background: #aba9a9;
   width: 80vw;
   float: none;
   display: flex;
@@ -49,7 +53,8 @@ span {
   align-items: center;
   margin: 3vh auto 1vh auto;
 }
-button{
+
+button {
   margin-top: 26vh;
   margin-left: 42vw;
   width: 30vw;
